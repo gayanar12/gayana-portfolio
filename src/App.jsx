b362d7b3b02aa483ef2f3fe40e4c85876d0a9843
Gayana <gayanareddy3@gmail.com>
2025-03-28 12:36:10 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import About from './props/About.jsx'
+import Contact from './props/Contact.jsx'
+import Projects from './props/Projects.jsx'
+import Footer from './props/Footer.jsx'
+import Header from './props/Header.jsx'
+
+function App() {
+
+
+  return (
+    <>
+        <Footer />
+        <Header />
+<About />
+        <Contact />
+        <Projects />
+
+
+
+    </>
+  )
+}
+
+export default App
