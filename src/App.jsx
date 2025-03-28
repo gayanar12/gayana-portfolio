589f7836480c94677fd6c799f96ee96f304f8a53
Gayana <gayanareddy3@gmail.com>
2025-03-28 12:54:06 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from './components/Header.jsx';
+import Header from './components/Header';
 import About from './components/About';
-import Projects from './components/Projects.jsx';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
